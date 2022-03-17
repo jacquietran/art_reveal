066b5008a442e6a5cd1ed8e5d500d0e65c15b758
Jacquie Tran <jacquie.tran@gmail.com>
2022-03-17 21:47:58 +1300

    Add grain functions

---
+++ R/fx_grain2.R
@@ -0,0 +1,19 @@
+mimic_grain2 <- function(seed_num, lower, upper, grain, n_lines, colours){
+  
+  # Requires {tibble} and {dplyr}
+  
+  weights <- c(0.2, 0.2, 0.15, 0.15, 0.05, 0.05, 0.05, 0.05, 0.02, 0.02, 0.02,
+               0.02, 0.01, 0.01)
+  
+  set.seed(seed_num)
+  data <- tibble::tibble(
+    x = rep(seq(lower, upper, length.out = grain), times = n_lines),
+    y = rep(seq(lower, upper, length.out = n_lines), each = grain)) |>
+    dplyr::mutate(
+      point_size = sample(
+        seq(0.05, 0.7, by = 0.05), dplyr::n(), replace = TRUE, prob = weights),
+      point_colour = sample(colours, dplyr::n(), replace = TRUE))
+  
+  return(data)
+  
+}
