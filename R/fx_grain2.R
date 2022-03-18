e247ee168653f18254cb43194383f56553df7958
Jacquie Tran <jacquie.tran@gmail.com>
2022-03-18 21:50:41 +1300

    Change point sizing and add deletion

--- R/fx_grain2.R
+++ R/fx_grain2.R
@@ -1,9 +1,18 @@
 mimic_grain2 <- function(seed_num, lower, upper, grain, n_lines, colours){
   
-  # Requires {tibble} and {dplyr}
+  # Requires {scales}, {tibble} and {dplyr}
   
-  weights <- c(0.2, 0.2, 0.15, 0.15, 0.05, 0.05, 0.05, 0.05, 0.02, 0.02, 0.02,
-               0.02, 0.01, 0.01)
+  weights <- tibble::tibble(
+    values_init = c(
+      rep(0.5, times = 10),
+      rep(0.3, times = 10),
+      rep(0.1, times = 5),
+      rep(0.05, times = 5))) |>
+    dplyr::mutate(
+      values_sum = sum(values_init),
+      values_pct = values_init / values_sum) |>
+    dplyr::select(values_pct) |>
+    dplyr::pull()
   
   set.seed(seed_num)
   data <- tibble::tibble(
@@ -11,9 +20,11 @@ mimic_grain2 <- function(seed_num, lower, upper, grain, n_lines, colours){
     y = rep(seq(lower, upper, length.out = n_lines), each = grain)) |>
     dplyr::mutate(
       point_size = sample(
-        seq(0.05, 0.7, by = 0.05), dplyr::n(), replace = TRUE, prob = weights),
+        seq(0.05, 1.5, by = 0.05), dplyr::n(), replace = TRUE, prob = weights),
       point_colour = sample(colours, dplyr::n(), replace = TRUE),
-      to_delete = sample(c("yes", "no"), dplyr::n(), replace = TRUE))
+      to_delete = sample(
+        c("yes", "no"), dplyr::n(), replace = TRUE, prob = c(0.85, 0.15))) |>
+    dplyr::filter(to_delete == "no")
   
   return(data)
   
