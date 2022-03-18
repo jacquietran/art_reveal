e51adc4801d5d619bd7af36582b6ebc2f3b1a693
Jacquie Tran <jacquie.tran@gmail.com>
2022-03-18 23:07:36 +1300

    Trying out weirder blobbies

---
+++ R/fx_drop_blobs_many.R
@@ -0,0 +1,39 @@
+drop_blobs_many <- function(
+  seed_num, bg, x_lower, x_upper, y_lower, y_upper){
+  
+  # Requires {scales}, {tibble}, and {dplyr}
+  
+  set.seed(seed_num)
+  num_groups <- sample(seq(5, 15, by = 1), 1)
+  
+  num_points <- num_groups * 4
+  
+  x_integer_vec <- seq(x_lower, x_upper, length.out = 10)
+  y_integer_vec <- seq(y_lower, y_upper, length.out = 10)
+  
+  set.seed(seed_num)
+  data <- tibble::tibble(
+    x = sample(x_integer_vec, num_points, replace = TRUE),
+    y = rep(sample(y_integer_vec, num_points / 2, replace = TRUE), each = 2),
+    group = rep(seq(1, num_groups, by = 1), each = 4))
+  
+  set.seed(seed_num)
+  rel_groups_sets <- data |>
+    dplyr::distinct(group) |>
+    dplyr::mutate(
+      set_num = sample(c(1, 2, 3), dplyr::n(), replace = TRUE))
+  
+  data_mod <- dplyr::left_join(data, rel_groups_sets, by = "group")
+  
+  set1 <- data_mod |>
+    dplyr::filter(set_num == 1)
+  
+  set2 <- data_mod |>
+    dplyr::filter(set_num != 1)
+  
+  return(list(
+    "combined" = data_mod,
+    "set1" = set1,
+    "set2" = set2))
+  
+}
---
+++ R/reveal_0043.R
@@ -0,0 +1,72 @@
+# Source custom functions ------------------------------------------------------
+
+source(here::here("R/fx_drop_blobs_many.R"))
+source(here::here("R/fx_grain2.R"))
+
+# Set parameters ---------------------------------------------------------------
+
+# General params
+iteration_id <- "reveal_0043"
+initial_seed <- 710043
+bg_colour <- "#F1ECCE"
+line_colour <- "#2F4858"
+grain_colours <- c("#3581B8", "#F97068", "#EBE9E9")
+lower_limit <- -1
+upper_limit <- 11
+
+# Create data ------------------------------------------------------------------
+
+# Create grain layer
+grain_data <- mimic_grain2(
+  seed_num = initial_seed, lower = lower_limit, upper = upper_limit,
+  grain = 1000, n_lines = 300, colours = grain_colours)
+
+# Drop blobs
+blobs <- drop_blobs_many(
+  seed_num = initial_seed, bg = bg_colour,
+  x_lower = 0, x_upper = 10, y_lower = 0, y_upper = 10)
+
+blobs_combined <- blobs$combined
+blobs_set1 <- blobs$set1
+
+# Build plot -------------------------------------------------------------------
+
+p <- ggplot2::ggplot() +
+  ggfx::as_reference(
+    ggforce::geom_diagonal_wide(
+      data = blobs_combined,
+      ggplot2::aes(x = x, y = y, group = group),
+      strength = 2.5, fill = bg_colour, size = 0, alpha = 1,
+      radius = ggplot2::unit(8, 'mm')),
+    id = "blobs") +
+  ggfx::with_mask(
+    ggplot2::geom_point(
+      data = grain_data,
+      ggplot2::aes(x = x, y = y, size = point_size, colour = point_colour),
+      shape = 16, stroke = 0, alpha = 0.55),
+    mask = ggfx::ch_alpha("blobs")) +
+  ggforce::geom_diagonal_wide(
+    data = blobs_combined,
+    ggplot2::aes(x = x, y = y, group = group),
+    strength = 2.5, colour = line_colour, size = 5, alpha = 0,
+    radius = ggplot2::unit(8, 'mm')) +
+  ggplot2::scale_colour_identity() +
+  ggplot2::scale_size_identity() +
+  ggplot2::coord_fixed(
+    xlim = c(lower_limit, upper_limit), ylim = c(lower_limit, upper_limit),
+    expand = TRUE) +
+  ggplot2::theme_void() +
+  ggplot2::theme(
+    legend.position = "none",
+    plot.background = ggplot2::element_rect(
+      fill = bg_colour, colour = bg_colour),
+    plot.margin = ggplot2::margin(40,40,40,40, unit = "pt"))
+
+# Export to file ---------------------------------------------------------------
+
+ggplot2::ggsave(
+  here::here(glue::glue("img/{`iteration_id`}.png")),
+  ggplot2::last_plot(), width = 9000, height = 9000, units = "px", dpi = 600,
+  device = ragg::agg_png)
+
+beepr::beep(2)
